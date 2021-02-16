e327dc1a22c8b5b2ae37a27d49cd478ac182f556
tuckerTU <tuckerishere@gmail.com>
2021-02-16 10:24:18 -0700

    fill out User model class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
 class User < ActiveRecord::Base
 
+    has_secure_password
+    has_many :reviews
+    has_many :movies, through: reviews
+
+    def self.invalid?(params)
+        true if User.find_by(username: params[:username]) || User.find_by(email: params[:email])
+    end
+
+    def slug
+        username.downcase.gsub(" ", "-")
+    end
+
+    def self.find_by_slug(slug)
+        User.all.find { |object| object.slug == slug }
+    end
+    
 end
