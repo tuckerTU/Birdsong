d01d847927cacd52a6e520cf72dce664b75f0451
tuckerTU <tuckerishere@gmail.com>
2021-02-18 12:18:22 -0700

    update user model with slugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,12 @@ class User < ActiveRecord::Base
     has_many :reviews
     has_many :movies, through: reviews
 
-    def self.invalid?(params)
-        true if User.find_by(username: params[:username]) || User.find_by(email: params[:email])
-    end
-
     def slug
         username.downcase.gsub(" ", "-")
     end
 
     def self.find_by_slug(slug)
-        User.all.find { |object| object.slug == slug }
+        User.all.find{|user| user.slug == slug}
     end
-    
+
 end
