810efbad548e22f0298d1a88ebabe5de799d8856
tuckerTU <tuckerishere@gmail.com>
2021-02-22 11:01:46 -0700

    add logged_in? and redirect... methods to helpers in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,6 +22,15 @@ class ApplicationController < Sinatra::Base
     def current_user
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
+
+    def logged_in?
+      !!current_user
+    end
+
+    def redirect_if_not_logged_in
+      if !logged_in?
+        redirect "/login"
+      end
   end
 
 end
