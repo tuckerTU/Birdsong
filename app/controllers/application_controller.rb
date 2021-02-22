3423fea115bf86a09b6e1e439d78488f9607e909
tuckerTU <tuckerishere@gmail.com>
2021-02-22 10:42:41 -0700

    add current user method to helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,5 +18,10 @@ class ApplicationController < Sinatra::Base
     redirect '/login'
   end
 
+  helpers do 
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+  end
 
 end
