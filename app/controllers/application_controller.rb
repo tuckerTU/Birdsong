ea3ba6667faeca6acf34ee6fdc9a9edf80a38e07
tuckerTU <tuckerishere@gmail.com>
2021-02-22 10:40:03 -0700

    enable sessions in configure method in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,14 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, ENV['SESSION_SECRET']
+    register Sinatra::Flash
   end
 
   get "/" do
     erb :welcome
   end
 
-  
 
 end
