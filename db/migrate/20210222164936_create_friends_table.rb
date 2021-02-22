e8136afb90c0a866d09eea9bd5c89af845253336
tuckerTU <tuckerishere@gmail.com>
2021-02-22 09:51:21 -0700

    add friend_id/added_friend_id to friends table

--- db/migrate/20210222164936_create_friends_table.rb
+++ db/migrate/20210222164936_create_friends_table.rb
@@ -1,4 +1,8 @@
 class CreateFriendsTable < ActiveRecord::Migration[5.2]
   def change
+    create_table :friends do |t|
+      t.integer :friend_id 
+      t.integer :added_friend_id
+    end
   end
 end
