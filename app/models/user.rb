b510a58296416d09fe0a02cbccbf774107f6cf35
tuckerTU <tuckerishere@gmail.com>
2021-02-23 09:23:49 -0700

    validate username uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
 
-    has_secure_password
     has_many :reviews
     has_many :albums, through: :reviews
     has_many :friends
+    has_secure_password
+    validates :username, uniqueness: true
 
 end
