e8109e05274d19be3c37ecb9782f2e0bc0cabbe6
tuckerTU <tuckerishere@gmail.com>
2021-02-23 09:28:23 -0700

    validate password length and confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
     has_many :albums, through: :reviews
     has_many :friends
     has_secure_password
+    validates :password, length: {in: 6..100}, confirmation: true
     validates :username, uniqueness: true
 
 end
