c678319db0689dd4724bc676fb676e2a585db48a
tuckerTU <tuckerishere@gmail.com>
2021-02-23 09:30:09 -0700

    validate email uniqueness and presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ActiveRecord::Base
-
     has_many :reviews
     has_many :albums, through: :reviews
     has_many :friends
     has_secure_password
     validates :password, length: {in: 6..100}, confirmation: true
     validates :username, uniqueness: true
-
+    validates :email, presence: true, uniqueness: true
 end
