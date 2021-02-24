eb1492923072c30a79c093cf42d7eff0d1a9f25d
tuckerTU <tuckerishere@gmail.com>
2021-02-24 10:01:38 -0700

    add secure sessions password, create users controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ActiveRecord::Base
     has_secure_password
     validates :password, length: {in: 6..100}, confirmation: true
     validates :username, uniqueness: true
-    validates :email, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true, format:  {with: /\A(?<username>[^@\s]+)@((?<domain_name>[-a-z0-9]+)\.(?<domain>[a-z]{2,}))\z/i}
 end
