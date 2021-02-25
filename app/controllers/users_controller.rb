a305ae3564778d08c79b51be6e5b9bba277c9b0f
tuckerTU <tuckerishere@gmail.com>
2021-02-25 13:41:07 -0700

    Fix login error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,30 +17,59 @@ class UsersController < ApplicationController
     end
   end
 
-    # GET: /users/5
   get "/profile" do
     erb :"/users/show.html"
   end
 
-  get '/login' do
+  get '/login' do 
+      if logged_in?
+          redirect "user/#{current_user.slug}"
+      else
+          erb :'/users/login'
+      end
+    end
+    
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+      if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        flash[:success] = "Successfully logged in!"
+        redirect "/profile"
+      else
+        flash[:error] = "Invalid credentials"           
+        redirect '/login'
+      end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect '/login'
+    else
+      redirect '/login'
+    end
+  end  
+
+  get '/login' do 
     if logged_in?
       redirect "user/#{current_user.slug}"
     else
       erb :'/users/login'
     end
   end
-
+  
   post '/login' do
     @user = User.find_by(username: params[:username])
-
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect "/user/#{@user.slug}"
+      flash[:success] = "Successfully logged in!"
+      redirect "/profile"
     else
+      flash[:error] = "Invalid credentials"           
       redirect '/login'
     end
   end
-
+  
   get '/logout' do
     if logged_in?
       session.clear
